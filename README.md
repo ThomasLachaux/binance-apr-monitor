89f1274207d593564adb9d3dca2661c457313a1f
Thomas de Lachaux <thomas.delachaux@gmail.com>
2024-04-03 23:39:06 +0200

    feat: add grafana dashboard

--- README.md
+++ README.md
@@ -1,14 +1,19 @@
 # Binance APR Monitor
 
-This is a simple script to get APR (Annual Percentage Rate) of Binance Earn products and push it to InfluxDB.
+This is a quick and dirty script to get APR (Annual Percentage Rate) of Binance Earn products and push it to InfluxDB.
 
-![Binance APR Monitor](./img/grafana.jpg)
+![Binance APR Monitor](./img/grafana.png)
 
 ## Motivation
 
 I want to know if the APR of Binance Earn products is really worth it. As the price can change rapidly. I may put the results in the readme later.
 
 ![Binance Earn](./img/binance.jpg)
+
+## Analysis
+The most interesting data is when the data is aggrated over 15d by mean. It gives a nice view if the APR are interesting or not for a short period.
+
+![Binance APR 15d](./img/grafana_aggr.png)
 
 ## How to use
 
--- main.py
+++ main.py
@@ -2,18 +2,33 @@ import requests
 from influxdb_client import InfluxDBClient
 from influxdb_client.client.write_api import SYNCHRONOUS
 from os import environ
+import time
 
+# Sunday, January 1, 2023 12:00:00 AM
+START_TIME = 1672531200000
 
-def get_price(coin):
-    response = requests.get(
-        f"https://www.binance.com/bapi/earn/v1/public/lending/daily/product/market-apr/List?productId={coin}"
-    )
+DELTA = 4000000000
+
+def get_price(coin, startTime, endTime):
+    url = f"https://www.binance.com/bapi/earn/v1/public/lending/daily/product/market-apr/List?productId={coin}"
+
+    if startTime:
+        url += f"&startTime={startTime}"
+    
+    if endTime:
+        url += f"&endTime={endTime}"
+
+    print("Request URL: ", url)
+    response = requests.get(url)
     response.raise_for_status()
 
     prices = response.json()["data"]
 
+    return_prices = []
+
     for price in prices:
-        yield f"flexible_apy,asset={price['asset']} apr={float(price['marketApr'])} {int(price['calcTime'])}000000"
+        return_prices.append(f"flexible_apy,asset={price['asset']} apr={float(price['marketApr'])} {int(price['calcTime'])}000000")
+    return return_prices
 
 
 with InfluxDBClient(
@@ -24,6 +39,17 @@ with InfluxDBClient(
     org = environ["INFLUXDB_ORG"]
 
     with client.write_api(write_options=SYNCHRONOUS) as api:
-        for price in get_price("BTC001"):
-            print("Write price: ", price)
-            api.write(bucket, org, price)
+
+        for coin in environ['COINS'].split(','):
+            print("Coin: ", coin)
+            startTime = START_TIME
+            while startTime < time.time() * 1000:
+                endTime = startTime + DELTA
+
+                prices = get_price(coin, startTime, endTime)
+                
+                for price in prices:
+                    print("Write price: ", price)
+                    api.write(bucket, org, price)
+
+                startTime = endTime
